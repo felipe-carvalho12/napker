33c8358d8c68ee60244fb54fd07bc4d75d724a9a
felipe-carvalho12 <67564586+felipe-carvalho12@users.noreply.github.com>
2020-11-16 04:16:09 -0300

    .

---
+++ frontend/build/precache-manifest.a2c1e53fc424f6e955515e32cad074bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e35e04636da6bbe5c8894a4e66f318a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8fea8ee3de3550c12fdf",
+    "url": "/static/css/main.d28742bd.chunk.css"
+  },
+  {
+    "revision": "a58f04cdce1697a651c6",
+    "url": "/static/js/2.0c585f73.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.0c585f73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fea8ee3de3550c12fdf",
+    "url": "/static/js/main.9dab52da.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
