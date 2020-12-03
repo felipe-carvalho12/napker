04260f293c0e1260b1a3058fdb95a4010aa15666
felipe-carvalho12 <67564586+felipe-carvalho12@users.noreply.github.com>
2020-12-02 21:07:47 -0300

    password recovery

---
+++ frontend/build/precache-manifest.0127084d2f6dcb89c8aba80e41869536.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f6fc949b21435dd43f5e569d300e8d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "edd13fcc3a33e244f8e4",
+    "url": "/static/css/main.c898e0a9.chunk.css"
+  },
+  {
+    "revision": "100bf407557ba75b5a75",
+    "url": "/static/js/2.493dc296.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.493dc296.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edd13fcc3a33e244f8e4",
+    "url": "/static/js/main.36907d8d.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
