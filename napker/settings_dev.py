2de5fcf163edbad87b9da0ce277e45f2752176de
Carlos1043 <compradetenis022020@gmail.com>
2020-12-11 15:07:21 -0800

    ss

--- napker/settings_dev.py
+++ napker/settings_dev.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     "corsheaders",
     "channels",
     "profiles",
+    'storages',
     "chat",
     "posts",
 ]
--- napker/settings_prod.py
+++ napker/settings_prod.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     "corsheaders",
     "channels",
     "profiles",
+    'storages',
     "chat",
     "posts",
 ]
@@ -176,3 +177,24 @@ SECURE_SSL_REDIRECT = True
 CONN_MAX_AGE = None
 
 django_heroku.settings(locals())
+
+# S3
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'mysite/static'),
+]
+
+AWS_ACCESS_KEY_ID = 'AKIAIT2Z5TDYPX3ARJBA'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'sibtc-assets'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+
+AWS_LOCATION = 'static'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+
+DEFAULT_FILE_STORAGE = 'mysite.storage_backends.MediaStorage'
