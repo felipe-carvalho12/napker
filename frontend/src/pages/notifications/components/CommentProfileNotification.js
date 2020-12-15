f48e04a6254d1b4a3541829ef0f55641e34c7f4c
Carlos1043 <compradetenis022020@gmail.com>
2020-12-15 19:10:30 -0300

    notification

--- frontend/src/pages/notifications/components/CommentProfileNotification.js
+++ frontend/src/pages/notifications/components/CommentProfileNotification.js
@@ -7,37 +7,36 @@ export default function CommentProfileNotification(props) {
     const comment = props.comment
     return (
         <li className="notification-row" key={comment.author.id}>
-            <div className="d-flex align-items-center">
-                <Link to={`/user/${comment.author.slug}`}>
-                    <img src={`${SERVER_URL}${comment.author.photo}`}
-                        className="profile-img-med"
-                        style={{ marginRight: '10px' }}
-                    />
-                </Link>
+            <div className="notification-icon-container">
                 <i class="fas fa-comment notification-comment" style={{ margin: '0' }} />
-                <p className="text-secondary d-inline-block">
-                    {comment.created.split('-').reverse().join('/')}
-                </p>
-                {props.children}
             </div>
-            <div className="notification-message">
-                <p>
-                    <Link to={`/user/${comment.author.slug}`} style={{ color: "#000" }}>
-                        @{comment.author.user.username}
-                    </Link> comentou seu post.
-                </p>
-            </div>
-            <div className="notification-comment-container">
+            <div className="notification-container">
+                <div className="notification-info-container">
+                    <Link to={`/user/${comment.author.slug}`}>
+                        <img src={`${SERVER_URL}${comment.author.photo}`}
+                            className="profile-img-med"
+                            style={{ marginRight: '10px' }}
+                        />
+                    </Link>
+                    <p className="text-secondary d-inline-block">
+                        {comment.created.split('-').reverse().join('/')}
+                        {" • "}
+                        {props.children}
+                        <Link to={`/user/${comment.author.slug}`} style={{ color: "#000" }}>
+                            @{comment.author.user.username}
+                        </Link> comentou seu post.
+                    </p>
+                </div>
                 <div className="notification-comment-content">
                     {comment.content}
                 </div>
-            </div>
-            <div className="btn-see-post-container">
-                <Link to={`/post/${comment.post.id}`}>
-                    <button className="btn btn-primary btn-see-post">
-                        Ver Post
-                    </button>
-                </Link>
+                <div className="btn-see-post-container">
+                    <Link to={`/post/${comment.post.id}`}>
+                        <button className="btn btn-primary btn-see-post">
+                            Ver Post
+                        </button>
+                    </Link>
+                </div>
             </div>
         </li>
     )
--- frontend/src/pages/notifications/components/LikeProfileNotification.js
+++ frontend/src/pages/notifications/components/LikeProfileNotification.js
@@ -8,37 +8,36 @@ export default function LikeProfileNotification(props) {
 
     return (
         <li className="notification-row" key={like.profile.id}>
-            <div className="d-flex align-items-center">
-                <Link to={`/user/${like.profile.slug}`}>
-                    <img src={`${SERVER_URL}${like.profile.photo}`}
-                        className="profile-img-med"
-                        style={{ marginRight: '10px' }}
-                    />
-                </Link>
+            <div className="notification-icon-container">
                 <i class="fas fa-heart notification-like" />
-                <p className="text-secondary d-inline-block" style={{ margin: '0' }}>
-                    {like.created.split('-').reverse().join('/')}
-                </p>
-                {props.children}
             </div>
-            <div className="notification-message">
-                <p>
-                    <Link to={`/user/${like.profile.slug}`} style={{ color: "#000" }}>
-                        @{like.profile.user.username}
-                    </Link> curtiu seu post.
-                </p>
-            </div>
-            <div className="notification-like-container">
+            <div className="notification-container">
+                <div className="notification-info-container">
+                    <Link to={`/user/${like.profile.slug}`}>
+                        <img src={`${SERVER_URL}${like.profile.photo}`}
+                            className="profile-img-med"
+                            style={{ marginRight: '10px' }}
+                        />
+                    </Link>
+                    <p className="text-secondary d-inline-block" style={{ margin: '0' }}>
+                        {like.created.split('-').reverse().join('/')}
+                        {" • "}
+                        {props.children}
+                        <Link to={`/user/${like.profile.slug}`} style={{ color: "#000" }}>
+                            @{like.profile.user.username}
+                        </Link> curtiu seu post.
+                    </p>
+                </div>
                 <div className="notification-post-content">
                     {like.post.content}
                 </div>
-            </div>
-            <div className="btn-see-post-container">
-                <Link to={`/post/${like.post.id}`}>
-                    <button className="btn btn-primary btn-see-post">
-                        Ver Post
+                <div className="btn-see-post-container">
+                    <Link to={`/post/${like.post.id}`}>
+                        <button className="btn btn-primary btn-see-post">
+                            Ver Post
                         </button>
-                </Link>
+                    </Link>
+                </div>
             </div>
         </li>
     )
