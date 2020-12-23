b15b4eb2985939f085c426edc4ec7380db388da0
felipe-carvalho12 <67564586+felipe-carvalho12@users.noreply.github.com>
2020-12-23 17:52:16 -0300

    deploy to heroku...

---
+++ frontend/build/precache-manifest.567a9a0c340ad672ca1b1507b6ed131e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "697d7b5903b690be241a3b2131f0b8de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d62b667734511b3dc31",
+    "url": "/static/css/2.c5b3f571.chunk.css"
+  },
+  {
+    "revision": "32fc1298fbee034ee42f",
+    "url": "/static/css/main.68f0dcd0.chunk.css"
+  },
+  {
+    "revision": "6d62b667734511b3dc31",
+    "url": "/static/js/2.121ccb7c.chunk.js"
+  },
+  {
+    "revision": "6a72c9d227f1391372cfca70564d05a6",
+    "url": "/static/js/2.121ccb7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32fc1298fbee034ee42f",
+    "url": "/static/js/main.257140ce.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
