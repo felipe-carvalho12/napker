fbf63fb7c1a21b8e3031f0c9415be912ef85f571
Carlos1043 <compradetenis022020@gmail.com>
2020-12-23 22:29:59 -0300

    colors

--- frontend/src/pages/profile/components/ProfileData.js
+++ frontend/src/pages/profile/components/ProfileData.js
@@ -22,8 +22,8 @@ export default function ProfileData(props) {
                     <i className="far fa-calendar-alt"></i> Entrou em {profile.created.split('-').reverse().join('/')}
                 </p>
                 <p>
-                    <Link to={`/user/${profile.slug}/amigos`} style={{ color: '#000' }}>
-                        <strong>{profile.friends.length}</strong> {profile.friends.length === 1 ? 'amigo' : 'amigos'}
+                    <Link to={`/user/${profile.slug}/amigos`} style={{color:  "var(--primary-grey)"}}>
+                        <strong>{profile.friends.length}</strong> <span className="text-secondary">{profile.friends.length === 1 ? 'amigo' : 'amigos'}</span>
                     </Link>
                 </p>
             </div>
