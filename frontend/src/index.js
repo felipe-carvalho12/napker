9fc67a3bbca695034a9d0d3af95e5240d85f4590
felipe-carvalho12 <67564586+felipe-carvalho12@users.noreply.github.com>
2020-12-25 04:19:07 -0300

    refactored assets folder architecture

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,20 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import './assets/style/auth.css'
-import './assets/style/bottomMenu.css'
-import './assets/style/chat.css'
-import './assets/style/commentModal.css'
-import './assets/style/explore.css'
-import './assets/style/animations.css'
-import './assets/style/home.css'
-import './assets/style/posts.css'
-import './assets/style/profile.css'
-import './assets/style/profiles.css'
-import './assets/style/settings.css'
-import './assets/style/sidebar.css'
-import './assets/style/style.css'
-import './assets/style/responsivity.css'
+import './assets/style/utils/hover.css'
+import './assets/style/utils/border.css'
+
+import './assets/style/pages/auth/auth.css'
+
+import './assets/style/pages/global-components/loader.css'
+import './assets/style/pages/global-components/modal.css'
+import './assets/style/pages/global-components/buttons.css'
+import './assets/style/pages/global-components/viewMoreSelect.css'
+
+import './assets/style/pages/main-app/components/bottomMenu.css'
+import './assets/style/pages/main-app/chat.css'
+import './assets/style/pages/main-app/components/commentModal.css'
+import './assets/style/pages/main-app/home/components/explore.css'
+import './assets/style/pages/main-app/home/home.css'
+import './assets/style/pages/main-app/home/components/posts.css'
+import './assets/style/pages/main-app/profile.css'
+import './assets/style/pages/main-app/home/components/profiles.css'
+import './assets/style/pages/main-app/settings.css'
+import './assets/style/pages/main-app/components/sidebar.css'
+
+import './assets/style/main/animations.css'
+import './assets/style/main/responsivity.css'
+import './assets/style/main/style.css'
 
 import AppContextProvider from './context/app/AppContext'
 import App from './App'
