8faeed7234959ad239f4dd03290ea17a051457a6
felipe-carvalho12 <67564586+felipe-carvalho12@users.noreply.github.com>
2020-12-26 03:25:04 -0300

    heroku

--- src/pages/main-app/messages_/Messages.js
+++ src/pages/main-app/messages_/Messages.js
@@ -7,7 +7,7 @@ import Chat from './components/chat_/Chat'
 import ContactListItem from './components/ContactListItem'
 import ContactFilterInput from './components/ContactFilterInput'
 import BottomMenu from '../../../components/fixed/bottom-menu/BottomMenu'
-import WebSocketInstance from './Websocket'
+import WebSocketInstance from './websocket'
 
 export default class Messages extends React.Component {
     constructor(props) {
