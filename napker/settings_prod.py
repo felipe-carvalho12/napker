1ecffd2eb2f4b1253ad082f83b7e6e4ddc3ab7f4
Carlos1043 <compradetenis022020@gmail.com>
2020-12-27 22:38:37 -0300

    s3

--- napker/settings_prod.py
+++ napker/settings_prod.py
@@ -194,3 +194,4 @@ AWS_LOCATION = 'media_root'
 
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage' 
+DEFAULT_FILE_STORAGE = 'napker.storage_backends.MediaStorage'
