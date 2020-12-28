2c9b2d4c069ed6705c4abfca7fab8cf17eb129cb
Carlos1043 <compradetenis022020@gmail.com>
2020-12-27 23:00:44 -0300

    s3

--- napker/settings_prod.py
+++ napker/settings_prod.py
@@ -141,7 +141,7 @@ STATIC_URL = "/static/"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
     os.path.join(BASE_DIR, "frontend/build/static"),
-    os.path.join(BASE_DIR, './media_root'),
+    os.path.join(BASE_DIR, 'media_root'),
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static_root")
