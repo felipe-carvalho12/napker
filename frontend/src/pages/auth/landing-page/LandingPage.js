1be148c2203f0eeb76193624e3830c17cbd02ffd
felipe-carvalho12 <felipebpbcarvalho@gmail.com>
2021-01-12 13:32:41 -0300

    lp

--- frontend/src/pages/auth/landing-page/LandingPage.js
+++ frontend/src/pages/auth/landing-page/LandingPage.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import DemoAddInterests from './components/DemoAddInterests'
 
+import Logo from '../../../assets/icons/Logo'
+import DemoAddInterests from './components/DemoAddInterests'
 import DemoAlgorithmSettings from './components/DemoAlgorithmSettings'
 import DemoSearchProfiles from './components/DemoSearchProfiles'
 import Wave from './components/Wave'
@@ -11,50 +12,72 @@ export default function LandingPage() {
     document.title = 'Bem Vindo / Napker'
 
     return (
-        <div className="d-flex flex-column align-items-center">
+        <div className="d-flex flex-column align-items-center" style={{ color: 'var(--lp-c0)', background: '#fff' }}>
             <div
-                className="d-flex flex-column w-100 justify-content-center align-items-center"
-                style={{ background: "linear-gradient(180deg,var(--primary-color-hover) -.45%,var(--primary-color) 20%)", padding: "50px" }}
+                className="position-fixed d-flex justify-content-center align-items-center w-100"
+                style={{ top: '0', left: '0', zIndex: '10000' }}
             >
-                <div className="d-flex flex-column w-50 justify-content-center align-items-center" style={{ textAlign: "center", padding: "20px" }}>
-                    <h2 style={{ color: "var(--theme-base-color)", fontSize: "60px", fontWeight: "700" }}>Por que usar o Napker?</h2>
-                    <div className="d-flex flex-column align-items-center">
-                        <h4 style={{ color: "var(--background)", fontSize: "20px" }}>
-                            As grandes redes socias utilizam o gigantesco volume de dados gerados pelos usuários para alimentar algoritmos misteriosos que não temos acesso.
-                            Esses algoritmos controlam a experiêcia do usuário na rede social visando mantê-lo o máximo de tempo possível no aplicativo, e criam modelos de usuários
-                            para vender para anunciantes. O Napker foi pensado como uma alternativa a essas redes sociais. Com um algoritmo personalizável e um
-                            sistema de interesses públicos e privados, o usuário tem total controle sobre a sua experiência na plataforma
-                        </h4>
-                        <div className="d-flex justify-content-between" style={{ width: "80%" }}>
-                            <Link to="/login">
-                                <button className="btn-call" style={{ color: "var(--primary-grey)", background: "var(--theme-base-color)" }}>
-                                    Já tem uma conta?
+                <div className="d-flex justify-content-between align-items-center w-75 p-3">
+                    <Logo />
+                    <div className="d-flex justify-content-between">
+                        <Link to="/login">
+                            <button className="btn btn-primary m-0 mr-2">
+                                Já tem uma conta?
+                        </button>
+                        </Link>
+                        <Link to="/signup">
+                            <button className="btn btn-primary m-0">
+                                Inscrever-se
+                        </button>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+            <div
+                className="d-flex flex-column w-100 justify-content-center align-items-center position-relative b-primary-color"
+                style={{ zIndex: '20000' }}
+            >
+                <div className="w-100" style={{ background: "linear-gradient(180deg,var(--lp-c4) -.45%,var(--primary-color) 40%)", padding: "50px" }}>
+                    <div className="d-flex flex-column w-150 justify-content-center align-items-center" style={{ textAlign: "center", padding: "20px" }}>
+                        <h2 style={{ color: "var(--lp-c1)", fontSize: "60px", fontWeight: "700" }}>Por que usar o Napker?</h2>
+                        <div className="d-flex flex-column align-items-center">
+                            <h4 style={{ color: "var(--lp-c2)", fontSize: "20px" }}>
+                                As grandes redes socias utilizam o gigantesco volume de dados gerados pelos usuários para alimentar algoritmos misteriosos que não temos acesso.
+                                Esses algoritmos controlam a experiêcia do usuário na rede social visando mantê-lo o máximo de tempo possível no aplicativo, e criam modelos de usuários
+                                para vender para anunciantes. O Napker foi pensado como uma alternativa a essas redes sociais. Com um algoritmo personalizável e um
+                                sistema de interesses públicos e privados, o usuário tem total controle sobre a sua experiência na plataforma
+                            </h4>
+                            <div className="d-flex justify-content-between p-3" style={{ width: "50%" }}>
+                                <Link to="/login">
+                                    <button className="btn-call" style={{ color: "var(--primary-grey)", background: "var(--theme-base-color)" }}>
+                                        Já tem uma conta?
                                 </button>
-                            </Link>
-                            <Link to="/signup">
-                                <button className="btn-call" style={{ color: "var(--theme-base-color)", background: "var(--primary-grey)" }}>
-                                    Inscrever-se
+                                </Link>
+                                <Link to="/signup">
+                                    <button className="btn-call" style={{ color: "var(--theme-base-color)", background: "var(--primary-grey)" }}>
+                                        Inscrever-se
                                 </button>
-                            </Link>
+                                </Link>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
             <div className="w-100 d-flex flex-column justify-content-center align-items-center">
                 <div className="w-100 d-flex flex-column justify-content-center align-items-center b-theme-base-color">
-                    <div className="w-100 px-2 d-flex justify-content-center" style={{ padding: '120px 0 90px' }}>
+                    <div className="w-100 px-2 d-flex justify-content-center" style={{ padding: '120px 0 90px', color: 'var(--lp-c1)' }}>
                         <DemoAlgorithmSettings />
                     </div>
                     <Wave color="var(--tertiary-grey)" />
                 </div>
                 <div className="w-100 d-flex flex-column justify-content-center align-items-center b-tertiary-grey">
-                    <div className="w-100 px-2 d-flex justify-content-center" style={{ padding: '120px 0 90px' }}>
+                    <div className="w-100 px-2 d-flex justify-content-center" style={{ padding: '120px 0 90px', color: 'var(--lp-c1)' }}>
                         <DemoAddInterests />
                     </div>
                     <Wave color="var(--theme-base-color)" />
                 </div>
                 <div className="w-100 d-flex justify-content-center b-theme-base-color">
-                    <div className="w-100 d-flex justify-content-center" style={{ padding: '120px 12.5% 90px' }}>
+                    <div className="w-100 d-flex justify-content-center" style={{ padding: '120px 12.5% 90px', color: 'var(--lp-c1)' }}>
                         <DemoSearchProfiles />
                     </div>
                     <Wave color="var(--primary-color)" />
@@ -69,7 +92,7 @@ export default function LandingPage() {
                     <h4 style={{ color: "var(--background)", fontSize: "20px", padding: '25px' }}>
                         Entre no grupo de testadores no Discord para acompanhar as novidades e discutir novas mudanças.
                     </h4>
-                    <div className="d-flex justify-content-between" style={{ width: "80%" }}>
+                    <div className="d-flex justify-content-between mt-2" style={{ width: "80%" }}>
                         <Link to="/signup">
                             <button className="btn-call" style={{ color: "var(--theme-base-color)", background: "var(--primary-grey)" }}>
                                 Inscrever-se no Napker
