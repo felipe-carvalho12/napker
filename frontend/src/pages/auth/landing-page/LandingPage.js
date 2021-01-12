0565c732cabe6a845f9cae92d58e8475b2c68059
Carlos1043 <compradetenis022020@gmail.com>
2021-01-12 18:36:30 -0300

    iiiiiiiiiiiiiii

--- frontend/src/pages/auth/landing-page/LandingPage.js
+++ frontend/src/pages/auth/landing-page/LandingPage.js
@@ -14,7 +14,7 @@ export default function LandingPage() {
     return (
         <div className="d-flex flex-column align-items-center c-lp-b-12 b-lp-w-12" style={{ color: 'var(--lp-b-12)', background: 'var(--lp-w-12)' }}>
             <div
-                className="position-fixed d-flex justify-content-center align-items-center w-100 b-lp-w-01 b-b blur"
+                className="position-fixed d-flex justify-content-center align-items-center w-100 b-lp-w-10 b-b blur"
                 style={{ top: '0', left: '0', zIndex: '10000', height: "50px" }}
             >
                 <div className="d-flex justify-content-between align-items-center w-75">
@@ -68,41 +68,41 @@ export default function LandingPage() {
                     <DemoAlgorithmSettings />
                 </div>
                 <div className="position-sticky w-100" style={{ top: '0', left: '0', zIndex: '1' }}>
-                    <Wave color="var(--lp-s-b-01)" />
-                    <div className="w-100 d-flex flex-column justify-content-center align-items-center b-lp-s-b-01">
+                    <Wave color="var(--lp-s-b-00)" />
+                    <div className="w-100 d-flex flex-column justify-content-center align-items-center b-lp-s-b-00">
                         <div className="w-100 px-2 d-flex justify-content-center" style={{ padding: '120px 0 90px', color: 'var(--lp-b-09)' }}>
                             <DemoAddInterests />
                         </div>
                     </div>
                 </div>
                 <div className="position-sticky w-100" style={{ top: '0', left: '0', zIndex: '2' }}>
-                    <Wave color="var(--theme-base-color)" />
-                    <div className="w-100 d-flex justify-content-center b-theme-base-color">
+                    <Wave color="var(--lp-s-b-01)" />
+                    <div className="w-100 d-flex justify-content-center b-lp-s-b-01">
                         <div className="w-100 d-flex justify-content-center" style={{ padding: '120px 12.5% 90px', color: 'var(--lp-b-09)' }}>
                             <DemoSearchProfiles />
                         </div>
                         <Wave color="var(--primary-color)" />
                     </div>
                 </div>
-            </div>
-            <div
-                className="d-flex flex-column w-100 justify-content-start align-items-center"
-                style={{ background: "linear-gradient(0deg,var(--primary-color-hover) -.45%,var(--primary-color) 50%)", padding: "50px", height: "300px" }}
-            >
-                <h2 style={{ color: "var(--theme-base-color)", fontSize: "40px", fontWeight: "700" }}>Gostou do que viu?</h2>
-                <div className="d-flex flex-column align-items-center">
-                    <h4 style={{ color: "var(--background)", fontSize: "20px", padding: '25px' }}>
-                        Entre no grupo de testadores no Discord para acompanhar as novidades e discutir novas mudanças.
-                    </h4>
-                    <div className="d-flex justify-content-between mt-2" style={{ width: "80%" }}>
-                        <Link to="/signup">
-                            <button className="s-b-l b-lp-b-09 c-lp-w-12">
-                                Inscrever-se no Napker
+                <div
+                    className="position-sticky d-flex flex-column w-100 justify-content-start align-items-center"
+                    style={{ top: '0', left: '0', zIndex: '3', background: "linear-gradient(0deg,var(--primary-color-hover) -.45%,var(--primary-color) 50%)", padding: "50px", height: "100vh" }}
+                >
+                    <h2 style={{ color: "var(--theme-base-color)", fontSize: "40px", fontWeight: "700" }}>Gostou do que viu?</h2>
+                    <div className="d-flex flex-column align-items-center">
+                        <h4 style={{ color: "var(--background)", fontSize: "20px", padding: '25px' }}>
+                            Entre no grupo de testadores no Discord para acompanhar as novidades e discutir novas mudanças.
+                        </h4>
+                        <div className="d-flex justify-content-between mt-2" style={{ width: "80%" }}>
+                            <Link to="/signup">
+                                <button className="s-b-l b-lp-b-09 c-lp-w-12">
+                                    Inscrever-se no Napker
+                                </button>
+                            </Link>
+                            <button className="s-b-l c-lp-w-12 b-lp-b-09">
+                                Entrar no grupo
                             </button>
-                        </Link>
-                        <button className="s-b-l c-lp-w-12 b-lp-b-09">
-                            Entrar no grupo
-                        </button>
+                        </div>
                     </div>
                 </div>
             </div>
