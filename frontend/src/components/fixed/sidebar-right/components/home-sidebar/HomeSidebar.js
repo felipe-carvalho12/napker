ca9a6fc891b9feec71d4b668ef5939ccd82eb4d9
fabiocarvalho20 <75388461+fabiocarvalho20@users.noreply.github.com>
2021-01-17 19:48:51 -0300

    style

--- frontend/src/components/fixed/sidebar-right/components/home-sidebar/HomeSidebar.js
+++ frontend/src/components/fixed/sidebar-right/components/home-sidebar/HomeSidebar.js
@@ -10,7 +10,7 @@ export default function FeedSidebar() {
     const pages = [<AlgorithmSettings />, 'Em breve...', 'Em breve...']
 
     return (
-        <div className="d-flex flex-column justify-content-between align-items-center" style={{ marginLeft: '10%' }}>
+        <div className="d-flex flex-column justify-content-between align-items-center h-100" style={{ marginLeft: '10%' }}>
             {pages[pageIndex]}
             <SidebarSlider handlePageChange={e => setPageIndex(e.target.id)} />
         </div>
