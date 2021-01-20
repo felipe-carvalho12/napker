b53ab3bd4f3b07865fc17bdc05ddd99d3fe93951
fabiocarvalho20 <75388461+fabiocarvalho20@users.noreply.github.com>
2021-01-20 17:01:40 -0300

    text

--- frontend/src/components/fixed/sidebar-right/components/home-sidebar/components/algorithm-settings/components/InfoModal.js
+++ frontend/src/components/fixed/sidebar-right/components/home-sidebar/components/algorithm-settings/components/InfoModal.js
@@ -15,21 +15,19 @@ export default function FeedAlgorithmInfoModal(props) {
             <Modal.Body>
                 <div className="d-flex flex-column b-theme-base-color p-2 b-b" style={{ marginBottom: "20px" }}>
                     <span className="mb-1 c-primary-grey">
-                        <p>Perfil: Os critérios presentes na barra lateral (Interesses em comum, Semelhança de idade...) são utilizados para calcularmos a
+                        <p><strong>Perfil:</strong> Os critérios presentes na barra lateral (Interesses em comum, Semelhança de idade...) são utilizados para calcularmos a
                         relevância de um perfil em relação a outro. Sabendo disso, você pode escolher o peso de cada critério e assim, personalizar o
                         algoritmo, que calculará a relevância dos perfia.</p>
-                        <p>Post: Os critérios presentes na barra lateral (Likes do post, Autor do post...) são utilizados para calcularmos a
-                        relevância de um post em relação a você. Sabendo disso, você pode escolher o peso de cada critério e assim, personalizar o
-                        algoritmo, que calculará a ordem dos posts no seu feed e no descobrir.</p>
+                        <p><strong>Post:</strong> A relevância de um post é calculada baseada na relevância do autor do mesmo e dos perfis que curtiram a publicação. Ou seja,
+                        ao decidir como a relevância de um perfil será calculada, você está personalizando o algoritmo que decidirá quais posts devem aparecer para você
+                        e em que ordem.</p>
                     </span>
                     <span className="mb-1 c-primary-grey">
                         <strong>Exemplo:</strong> Caso você mude o peso do critério "Interesses em comum" para 100 e todos os outros para 0, os perfis da página "Encontrar perfis" 
                         serão ordenados levando em consideração apenas a quantidade de interesses em comum que o usuário tem com você.
                     </span>
                     <span className="mb-1 c-primary-grey">
-                        A relevância de um post é calculada baseada na relevância do autor do mesmo e dos perfis que curtiram a publicação. Ou seja,
-                        ao decidir como a relevância de um perfil será calculada, você está personalizando o algoritmo que decidirá quais posts devem aparecer para você
-                        e em que ordem.
+            
                     </span>
                 </div>
             </Modal.Body>
