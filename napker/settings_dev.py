7681f3a964ded56aee0ae8ecfaef6fc1195eadaa
Carlos1043 <compradetenis022020@gmail.com>
2021-02-04 17:06:15 -0300

    dwadawdaw

--- napker/settings_dev.py
+++ napker/settings_dev.py
@@ -148,9 +148,11 @@ CORS_ALLOWED_ORIGINS = [
 
 # SMTP Configuration
 
+
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST = "smtp.umbler.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "napkercontato@gmail.com"
+EMAIL_HOST_USER = "contato@napker.com.br"
 EMAIL_HOST_PASSWORD = "dj@ng0Npkr8"
+
